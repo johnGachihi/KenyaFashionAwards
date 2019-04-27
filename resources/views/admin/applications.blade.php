d7ba8a9c5f7926ada939bb46e1c417f968ac84ac
johnGachihi <John.Waithaka@O365.strathmore.edu>
2019-04-27 18:39:29 +0300

    Added incomplete applications page template II

--- resources/views/admin/applications.blade.php
+++ resources/views/admin/applications.blade.php
@@ -15,41 +15,43 @@
             <div class="col-12 mb-4">
                 <div class="card mdc-elevation--z3">
                     <div class="card-body">
-                        <h5 class="card-title mb-3">Fashion Designer (Male)</h5>
+                        <h5 class="card-title mb-3 applicant-dets-titles">Fashion Designer (Male) Application</h5>
                         <div class="d-flex mb-3 row">
                             <div class="col-3">
-                                <span style="font-size: 11px; display: block; letter-spacing: 3px;">Applicant name:</span>
+                                <span class="applicant-dets-titles" style="font-size: 11px; display: block; letter-spacing: 3px;">Applicant name:</span>
                                 <span>Charles Xavier</span>
                             </div>
                             <div class="col-3">
-                                <span style="font-size: 11px; display: block; letter-spacing: 3px;">Company name:</span>
+                                <span class="applicant-dets-titles" style="font-size: 11px; display: block; letter-spacing: 3px;">Company name:</span>
                                 <span>Nyeri Fashion and Design</span>
                             </div>
                         </div>
 
-                        {{-- Collapsible --}}
-                        <div class="collapse" id="collapsibleSection">
-                            <div class="row mb-3">
-                                <div class="col-3">
-                                    <span style="font-size: 11px; display: block; letter-spacing: 3px;">Email:</span>
-                                    <span>charles.xavier@gmail.com</span>
-                                </div>
-                                <div class="col-3">
-                                    <span style="font-size: 11px; display: block; letter-spacing: 3px;">Phone number:</span>
-                                    <span >+254 712 345678 </span>
-                                </div>
+                        <div class="row mb-3">
+                            <div class="col-3">
+                                <span class="applicant-dets-titles" style="font-size: 11px; display: block; letter-spacing: 3px;">Email:</span>
+                                <span>charles.xavier@gmail.com</span>
                             </div>
+                            <div class="col-3">
+                                <span class="applicant-dets-titles" style="font-size: 11px; display: block; letter-spacing: 3px;">Phone number:</span>
+                                <span >+254 712 345678 </span>
+                            </div>
+                        </div>
+
+                        {{-- Collapsible --}}
+                        <div class="collapse mb-3" id="collapsibleSection">
                             <div class="row">
                                 <div class="col-7">
-                                    <span style="font-size: 11px; display: block; letter-spacing: 3px;">Bio:</span>
+                                    <span class="applicant-dets-titles" style="font-size: 11px; display: block; letter-spacing: 3px;">Bio:</span>
                                     <span >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
                                 </div>
                             </div>
                         </div>
+                        {{-- Collapsible --}}
 
                         <div>
-                            <a data-toggle="collapse" href="#collapsibleSection">
-                                <i class="material-icons mdc-elevation--z2">expand_more</i>
+                            <a data-toggle="collapse" href="#collapsibleSection" id="collapse-btn">
+                                <i class="material-icons mdc-elevation--z3 rounded-circle" id="collapse-icon">expand_more</i>
                             </a>
                         </div>
                     </div>
@@ -57,4 +59,19 @@
             </div>
         </div>
     </div>
+@endsection
+
+@section('_scripts')
+    <script>
+        const collapseBtn = document.getElementById('collapse-btn');
+        const collapseIcon = document.getElementById('collapse-icon');
+
+        collapseBtn.addEventListener('click', e => {
+            if(collapseIcon.innerText == 'expand_more') {
+                collapseIcon.innerText = 'expand_less';
+            } else {
+                collapseIcon.innerText = 'expand_more'
+            }
+        });
+    </script>
 @endsection
