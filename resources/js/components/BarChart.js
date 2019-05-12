876b05e00e58e139002173f83d2c83684dd711ab
johnGachihi <John.Waithaka@O365.strathmore.edu>
2019-05-12 22:57:50 +0300

    Removed unnecessary plusOne method

--- resources/js/components/BarChart.js
+++ resources/js/components/BarChart.js
@@ -1,28 +1,32 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var chartjsConfig_1 = require("../components/chartjsConfig");
 var chart_js_1 = require("chart.js");
+var SideBarChartConfig_1 = require("./ChartConfig/SideBarChartConfig");
 var BarChart = /** @class */ (function () {
     function BarChart(awardCategoryData, canvasEl, incrementer) {
         this.incrementer = incrementer;
         this.awardCategoryData = awardCategoryData;
         this.canvasEl = canvasEl;
         var ctx = this.canvasEl.getContext('2d');
-        this.chart = new chart_js_1.Chart(ctx, chartjsConfig_1.getChartjsConfig(this.awardCategoryData.votes, this.awardCategoryData.candidates));
+        this.chart = new chart_js_1.Chart(ctx, new SideBarChartConfig_1.default(this.awardCategoryData.votes, this.awardCategoryData.candidates).getConfig());
         console.log(this.chart);
     }
-    BarChart.prototype.plusOne = function (candidate) {
-        var index = this.awardCategoryData.candidate_ids.indexOf(candidate.id);
-        if (index === -1) {
-            this.chart.data.datasets[0].data.push(1);
-            this.chart.data.labels.push(candidate.name);
-            this.awardCategoryData.candidate_ids.push(candidate.id);
-        }
-        this.chart.data.datasets[0].data[index] += 1;
-        this.chart.update();
+    BarChart.prototype.getChartData = function () {
+        return this.awardCategoryData;
     };
+    // plusOne(candidate : Candidate) {
+    //     const index = this.awardCategoryData.candidate_ids.indexOf(candidate.id);
+    //     if(index === -1) {
+    //         this.chart.data.datasets[0].data.push(1);
+    //         this.chart.data.labels.push(candidate.name);
+    //         this.awardCategoryData.candidate_ids.push(candidate.id);
+    //     }
+    //     this.chart.data.datasets[0].data[index] += 1;
+    //     this.chart.update();
+    // }
     BarChart.prototype.increment = function (candidate) {
         this.incrementer.incrementDataPoint(this.chart, candidate, this.awardCategoryData);
+        console.log(this.awardCategoryData);
     };
     return BarChart;
 }());
--- resources/js/components/BarChart.ts
+++ resources/js/components/BarChart.ts
@@ -1,45 +1,52 @@
 import {getChartjsConfig} from '../components/chartjsConfig'
+import {getSideBarChartConfig} from '../components/chartjsConfig'
 import {Chart} from 'chart.js'
 import SideBarChartDataPointIncrementer from "./DataPointIncrementer/SideBarChartDataPointIncrementer";
 import DataPointIncrementer from "./DataPointIncrementer/DataPointIncrementer";
 import Candidate from "./Interfaces/Candidate";
 import VotesInCategory from "./Interfaces/VotesInCategory";
+import SideBarChartConfig from "./ChartConfig/SideBarChartConfig";
 
 export default class BarChart {
     awardCategoryData: VotesInCategory;
     canvasEl: HTMLCanvasElement;
-    // totalEl: HTMLElement;
     chart: Chart;
 
     incrementer: DataPointIncrementer;
 
-    constructor(awardCategoryData, canvasEl, incrementer : DataPointIncrementer) {
+    constructor(awardCategoryData, canvasEl, incrementer: DataPointIncrementer) {
         this.incrementer = incrementer;
 
         this.awardCategoryData = awardCategoryData;
         this.canvasEl = canvasEl;
 
         const ctx = this.canvasEl.getContext('2d');
-        this.chart = new Chart(ctx, getChartjsConfig(
+        this.chart = new Chart(ctx, new SideBarChartConfig(
             this.awardCategoryData.votes,
             this.awardCategoryData.candidates
-        ));
+        ).getConfig());
+
         console.log(this.chart);
     }
 
-    plusOne(candidate : Candidate) {
-        const index = this.awardCategoryData.candidate_ids.indexOf(candidate.id);
-        if(index === -1) {
-            this.chart.data.datasets[0].data.push(1);
-            this.chart.data.labels.push(candidate.name);
-            this.awardCategoryData.candidate_ids.push(candidate.id);
-        }
-        this.chart.data.datasets[0].data[index] += 1;
-        this.chart.update();
+    getChartData() {
+        return this.awardCategoryData;
     }
+
+    // plusOne(candidate : Candidate) {
+    //     const index = this.awardCategoryData.candidate_ids.indexOf(candidate.id);
+    //     if(index === -1) {
+    //         this.chart.data.datasets[0].data.push(1);
+    //         this.chart.data.labels.push(candidate.name);
+    //         this.awardCategoryData.candidate_ids.push(candidate.id);
+    //     }
+    //     this.chart.data.datasets[0].data[index] += 1;
+    //     this.chart.update();
+    // }
 
     increment(candidate : Candidate) : void {
         this.incrementer.incrementDataPoint(this.chart, candidate, this.awardCategoryData);
+        console.log(this.awardCategoryData);
     }
 
 }
