1245404044b53649edad4db281a25bc1a6d26361
jsscmaina <43575617+jsscmaina@users.noreply.github.com>
2019-06-01 18:38:01 +0300

    Update Article.php

--- app/Article.php
+++ app/Article.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    //Changes the settings in the database
+    protected $table ='articles';
+    public $primaryKey = 'id';
+    public $timestamps = true;
+
+    public function user(){
+    	return $this->belongsTo('App\User');
+    }
 }
